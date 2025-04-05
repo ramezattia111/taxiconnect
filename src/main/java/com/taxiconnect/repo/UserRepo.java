2253ba8ce51e484dbf706b4a4299ba5e7a1d3178
ramezattia111 <ramezattia224@gmail.com>
2025-04-05 11:54:59 +0000

    move file folder

--- src/main/java/com/taxiconnect/repo/UserRepo.java
+++ src/main/java/com/taxiconnect/repo/UserRepo.java
@@ -2,7 +2,7 @@ package com.taxiconnect.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.taxiconnect.entities.User;
+import com.taxiconnect.entities.user.User;
 
 public interface UserRepo extends JpaRepository<User, Long>{
 
